5c51f2e0cabdc04e034e0a4a332fec64cba4a0bd
makitow <makito@caramel-inc.com>
2021-09-10 18:40:06 +0900

    ボタン押せないバグ修正

--- _nuxt/static/1631265234/company/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631265234",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcompany",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"73177df2"}},__i18n:{langs:{}}}}(null));
--- _nuxt/static/1631265234/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmarketing","\u002Fdesign","\u002Fcompany","\u002Fnewbiz","\u002Fservice"]})
