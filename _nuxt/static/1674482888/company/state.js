d38f5001cbfa1950cc67b4eb61a2e12103194a5e
makitow <makito@caramel-inc.com>
2024-04-11 14:37:28 +0900

    住所変更

--- _nuxt/static/1674482888/company/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674482888",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcompany",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"44926a78"}},__i18n:{langs:{}}}}(null));
--- _nuxt/static/1674482888/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fdesign","\u002F","\u002Fmarketing","\u002Fnewbiz","\u002Fservice","\u002Fcompany"]})
